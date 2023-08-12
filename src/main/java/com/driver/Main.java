660ce9480e17e8af1ceac2537c6671c5cf593515
Ashekhawat@2001 <ankush75056@gmail.com>
2023-08-12 17:11:52 +0530

    atm1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String args[])
     {
         RWOnly obj = new RWOnly();
-        obj.name = "Ankush";
+        //obj.name = "Ankush";
         // name has private variable this not directly assign value need setters
          obj.setName("Ankush");
          obj.getName();
