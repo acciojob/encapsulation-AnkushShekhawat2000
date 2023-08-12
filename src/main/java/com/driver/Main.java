82e4e4b4012d2b9934ef395f226d7ebcc4e6fad2
Ashekhawat@2001 <ankush75056@gmail.com>
2023-08-12 17:02:41 +0530

    assignment encapsulation complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[])
+    {
+        RWOnly obj = new RWOnly();
+        obj.name = "Ankush";
+        // name has private variable this not directly assign value need setters
+         obj.setName("Ankush");
+         obj.getName();
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    public String setName(String name)
+    {
+        return name;
+    }
+
+    public void getName()
+    {
+        this.name = name;
+    }
 }
